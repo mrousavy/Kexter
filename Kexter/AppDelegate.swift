237884c8374da33cb31e3663c5786f6f0cba238e
mrousavy <mrousavy@Marcs-MacBook-Pro.local>
2017-05-14 09:47:18 +0200

    Added XCode Project

---
+++ Kexter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Kexter
+//
+//  Created by Marc Rousavy on 14/05/2017.
+//  Copyright © 2017 mrousavy. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
